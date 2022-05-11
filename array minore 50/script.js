e897ed94155575f80591d1aacc7bc5589c68ebc8
Gloria <gloghe@yahoo.it>
2022-05-11 17:13:35 +0200

    fatto con recap

--- array minore 50/script.js
+++ array minore 50/script.js
@@ -1,20 +1,36 @@
-// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
+let myarray = [];
+let sommanumuser = 0;
 
-const myarray = [];
-
-
-
-while ( myarray <= 50 ) {
-
-let numerouser = parseInt(prompt("Scrivi un numero"));
-
-if(myarray >= 50){
-    let somma = numerouser + numerouser;
-
+do {
+     sommanumuser = 0;
+     
+    let numerouser = parseInt(prompt("Scrivi un numero"));
     myarray.push(numerouser);
-} 
+
+    for (let i = 0; i <myarray.length; i++){
+        sommanumuser += myarray[i];
+    }
+
+} while (sommanumuser<50);
+
+console.log(myarray)
 
 
-console.log(myarray);
-}
+
+     // const myarray = [];
+     // let sommanumuser = 0;
+     
+     // do {
+     
+     //     let numerouser = parseInt(prompt("Scrivi un numero"));
+     //     myarray.push(numerouser);
+     //     sommanumuser  += numerouser
+     
+     // } while (sommanumuser<50);
+     
+     // console.log(myarray)
+     
+     //
